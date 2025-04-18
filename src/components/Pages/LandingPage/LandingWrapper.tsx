e5d636fb4f096cea61fddc4400b32fc98b685d05
Chamuditha <lilan.2019660@iit.ac.lk>
2025-04-18 22:05:50 +0530

    landing page wrapper

---
+++ src/components/Pages/LandingPage/LandingWrapper.tsx
@@ -0,0 +1,17 @@
+import LandingSectionFour from "./LandingSectionFour"
+import LandingSectionOne from "./LandingSectionOne"
+import LandingSectionThree from "./LandingSectionThree"
+import LandingSectionTwo from "./LandingSectionTwo"
+
+function LandingWrapper() {
+    return (
+        <>
+            <LandingSectionOne></LandingSectionOne>
+            <LandingSectionTwo></LandingSectionTwo>
+            <LandingSectionThree></LandingSectionThree>
+            <LandingSectionFour></LandingSectionFour>
+        </>
+    )
+}
+
+export default LandingWrapper;
