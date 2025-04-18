b42321b82d8cc398690ef42f5681d61e87d8c3ba
Chamuditha <lilan.2019660@iit.ac.lk>
2025-04-18 22:24:44 +0530

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import './App.css'
-import Footer from './components/Layouts/Footer'
-import Navbar from './components/Layouts/Navbar'
-import LandingWrapper from './components/Pages/LandingPage/LandingWrapper'
+import './App.css';
+import Footer from './components/Layouts/Footer';
+import Navbar from './components/Layouts/Navbar';
+import LandingWrapper from './components/Pages/LandingPage/LandingWrapper';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
-
   return (
-    <>
-     <Navbar></Navbar>
-     <LandingWrapper></LandingWrapper>
-     <Footer></Footer>
-    </>
-  )
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<LandingWrapper />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
